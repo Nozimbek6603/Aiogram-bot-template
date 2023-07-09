615bd21e10519451460ffe6bc77c9d06e9e88b4f
Nozimbek6603 <nozimbek6603@gmail.com>
2023-07-09 16:09:32 +0500

    Aiogram_shablon fix and update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
+    # Birlamchi komandalar (/start va /help)
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
